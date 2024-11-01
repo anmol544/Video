f96f341ca743ae118a2603ebe70ff7029209a10c
anmol <thisanmolraj@gmail.com>
2024-11-01 14:55:48 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <h1>Auth App</h1>
-        <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+      <div style={{ textAlign: 'center', marginTop: '2rem', marginBottom:'2rem' }}>
+        <h1 className="headApp">Academics App</h1>
+        <Link to="/Pages/Login/Login"  style={{ marginRight: '1rem', color:'black',textDecoration:"none" }}>Login</Link> | <Link to="/Pages/Signup/Signup"  style={{ marginLeft: '1rem',color:'black', textDecoration:"none"  }}>Signup</Link>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/pages/Login/Login" element={<Login />} />
+          <Route path="/pages/Signup/Signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
